59149f5819177a5d63a1f498e816601931d0c96c
Andrzej Witkowski <endrjus8@gmail.com>
2020-05-23 10:40:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            SelectionSort.Sort(integerValues);
+            Console.WriteLine(string.Join(" | ", integerValues));
+
+            Console.ReadKey();
+        }
+    }
+}
