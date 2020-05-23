71b146174e15a4e9f94a84570fccc91c21780838
Andrzej Witkowski <endrjus8@gmail.com>
2020-05-23 14:05:17 +0200

    Add InsertionSort

--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@ namespace SortingAlgorithms
     {
         private static void Main()
         {
+            int[] integerValuesOriginal = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+
             int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+            int[] arr2 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            Console.Write("Before sorting:".PadRight(25));
+            Console.WriteLine(string.Join(" | ", integerValues));
+
+
             SelectionSort.Sort(integerValues);
+            Console.Write($"After {nameof(SelectionSort)}:".PadRight(25));
+            Console.WriteLine(string.Join(" | ", integerValues));
+
+            Array.Copy(integerValuesOriginal, integerValues, integerValues.Length);
+
+            InsertionSort.Sort(integerValues);
+            Console.Write($"After {nameof(InsertionSort)}:".PadRight(25));
             Console.WriteLine(string.Join(" | ", integerValues));
 
             Console.ReadKey();
